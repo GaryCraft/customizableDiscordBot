43f719f064de42f543958ac05db9b82d1a4a322c
GaryCraft <garycraft@our-space.xyz>
2024-04-23 01:21:39 -0600

    Show missing in i18n event

--- src/modules/i18n/hooks/missingKey.ts
+++ src/modules/i18n/hooks/missingKey.ts
@@ -1,6 +1,6 @@
 import { debug, warn } from "@src/engine/utils/Logger";
 import { I18nModule } from "../module";
 
-export default async (i18n: I18nModule) => {
-	warn("Missing key in translation file")
+export default async (i18n: I18nModule, lngs: string[], ns: string, msg: string) => {
+	warn("Missing key in translation file", { lngs, ns, msg });
 };
