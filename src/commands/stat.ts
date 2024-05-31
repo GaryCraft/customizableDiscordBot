6b97bff19bd8b9556eae3a4bca64cda8bba71f1a
GaryCraft <garycraft@our-space.xyz>
2024-05-31 16:14:00 -0600

    Stat command now shows hooks and tasks

--- src/commands/stat.ts
+++ src/commands/stat.ts
@@ -5,7 +5,7 @@ import { clear } from "@src/engine/utils/Logger";
 export default {
 	name: "stat",
 	description: "Displays Values about the application",
-	usage: "stat ",
+	usage: "stat <uptime|memory|commands|routes|modules|hooks|tasks>",
 	execute: async (app: ApplicationContext, args: string[]) => {
 		const uptime = process.uptime();
 		const memoryUsage = process.memoryUsage();
@@ -52,6 +52,26 @@ export default {
 				clear(table.join("\n"));
 				break;
 			}
+			case "hooks": {
+				const hooksSize = app.events.eventNames().length;
+				const table = [];
+				table.push(`${hooksSize} hooks loaded`);
+				for (const event of app.events.eventNames()) {
+					table.push(event);
+				}
+				clear(table.join("\n"));
+				break;
+			}
+			case "tasks": {
+				const tasksSize = app.tasks.jobs.size;
+				const table = [];
+				table.push(`${tasksSize} tasks loaded`);
+				for (const [name,t] of app.tasks.jobs) {
+					table.push(`${name} at ${t.cronInterval}`);
+				}
+				clear(table.join("\n"));
+				break;
+			}
 
 			default: {
 				const table = [
@@ -61,12 +81,12 @@ export default {
 				const cmdSize = app.cli.commands.size
 				const routesSize = app.http.server._router.length;
 				const modulesSize = app.modman.modules.size;
-				//const hooksSize = app.events.listeners.length;
+				const hooksSize = app.events.eventNames().length;
 				const tasksSize = app.tasks.jobs.size;
 				table.push(`${cmdSize} commands loaded`);
 				table.push(`${routesSize} routes loaded`);
 				table.push(`${modulesSize} modules loaded`);
-				//table.push(`${hooksSize} hooks loaded`);
+				table.push(`${hooksSize} hooks loaded`);
 				table.push(`${tasksSize} tasks loaded`);
 				clear(table.join("\n"));
 			}
