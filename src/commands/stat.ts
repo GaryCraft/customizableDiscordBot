9939bc01d89df1859c77e88a95211d95a624ed20
GaryCraft <garycraft@our-space.xyz>
2024-05-31 15:04:41 -0600

    Add stat command

---
+++ src/commands/stat.ts
@@ -0,0 +1,75 @@
+import { ApplicationContext } from "@src/engine/types/Engine";
+import { CliCommand } from "@src/engine/types/Executors";
+import { clear } from "@src/engine/utils/Logger";
+
+export default {
+	name: "stat",
+	description: "Displays Values about the application",
+	usage: "stat ",
+	execute: async (app: ApplicationContext, args: string[]) => {
+		const uptime = process.uptime();
+		const memoryUsage = process.memoryUsage();
+
+		let sub;
+		sub = args[0];
+		switch (sub) {
+			case "uptime": {
+				clear(`Uptime: ${uptime} seconds`);
+				break;
+			}
+			case "memory": {
+				clear(`Memory Usage: ${memoryUsage.rss} bytes`);
+				break;
+			}
+			case "commands": {
+				const cmdSize = app.cli.commands.size
+				const table = [];
+				table.push(`${cmdSize} commands loaded`);
+				for (const [cmd, command] of app.cli.commands) {
+					table.push(`(${cmd}) ${command.usage} - ${command.description}`);
+				}
+				clear(table.join("\n"));
+				break;
+			}
+			case "routes": {
+				const routesSize = app.http.server._router.length;
+				const table = [];
+				table.push(`${routesSize} routes loaded`);
+				for (const layer of app.http.server._router.stack) {
+					if(!layer.route) continue;
+					table.push(`${layer.route.path}`);
+				}
+				clear(table.join("\n"));
+				break;
+			}
+			case "modules": {
+				const modulesSize = app.modman.modules.size;
+				const table = [];
+				table.push(`${modulesSize} modules loaded`);
+				for (const [name,] of app.modman.modules) {
+					table.push(name);
+				}
+				clear(table.join("\n"));
+				break;
+			}
+
+			default: {
+				const table = [
+					`Uptime: ${uptime} seconds`,
+					`Memory Usage: ${memoryUsage.rss} bytes`
+				];
+				const cmdSize = app.cli.commands.size
+				const routesSize = app.http.server._router.length;
+				const modulesSize = app.modman.modules.size;
+				//const hooksSize = app.events.listeners.length;
+				const tasksSize = app.tasks.jobs.size;
+				table.push(`${cmdSize} commands loaded`);
+				table.push(`${routesSize} routes loaded`);
+				table.push(`${modulesSize} modules loaded`);
+				//table.push(`${hooksSize} hooks loaded`);
+				table.push(`${tasksSize} tasks loaded`);
+				clear(table.join("\n"));
+			}
+		}
+	}
+} satisfies CliCommand;
